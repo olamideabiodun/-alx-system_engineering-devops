4df01d6a051636f49a535ace477bdcd80ea6deaa
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-06-20 16:21:36 +0300

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 
-"""imports urllib.request to use the request module, imports the json to jsonify the fetched data"""
+"""imports urllib.request to use the request module,
+    imports the json to jsonify the fetched data"""
 import requests
 import json
 import sys
 
 
 if __name__ == "__main__":
-    all_tasks  = []
+    all_tasks = []
     numb = 0
     user_id = int(sys.argv[1])
-    response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
+    response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(user_id)).json()
 
     users = response.get("name", "username")
 
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
 
     for t in tasks:
-        if t.get("userId") == user_id:
+        if t.get("userId") is user_id:
             all_tasks.append(t)
-            if t.get("completed") == True:
+            if t.get("completed") is True:
                 numb += 1
-    print("Employee {} is done with tasks ({}/{})".format(users, numb, (len(all_tasks))))
+    print("Employee {} is done with tasks ({}/{})".format(users, numb,
+                                                          (len(all_tasks))))
     for t in all_tasks:
-        if t.get("completed") == True:
+        if t.get("completed") is True:
             title = t.get("title")
             print(title)
-       
-
