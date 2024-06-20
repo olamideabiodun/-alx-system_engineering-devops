670c6647922041fce6ab4478d126de5d52bae011
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-06-20 16:08:25 +0300

    task 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+## imports urllib.request to use the request module, imports the json to jsonify the fetched data
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    all_tasks  = []
+    numb = 0
+    user_id = int(sys.argv[1])
+    response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
+
+    users = response.get("name", "username")
+
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+
+    for t in tasks:
+        if t.get("userId") == user_id:
+            all_tasks.append(t)
+            if t.get("completed") == True:
+                numb += 1
+    print("Employee {} is done with tasks ({}/{})".format(users, numb, (len(all_tasks))))
+    for t in all_tasks:
+        if t.get("completed") == True:
+            title = t.get("title")
+            print(title)
+       
+
