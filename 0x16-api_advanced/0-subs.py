83b1c3b7815d459a98c03e20c2e8fcfb77efaebe
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-06-20 18:27:24 +0300

    subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""returns number of subreddit subscribers"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """gets number of subscribers to a certain reddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent':
+               'script:subs/1.0 by /u/faruqolamide'}
+    
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    else:
+        return 0
