151551cb857b8ec6698a53b6726d3c2c5f4eb87b
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-06-23 15:37:32 +0300

    top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,19 +3,22 @@
 
 import requests
 
-def number_of_subscribers(subreddit):
+
+def top_ten(subreddit):
     """gets number of subscribers to a certain reddit"""
 
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent':
                'script:subs/1.0 by /u/faruqolamide'}
-    
+
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         if res.status_code == 200:
-            titles = [item['data']['title']]
-            for item in titles:
-                
-        return res.json()['data']['children'][i]['data']['title']
+            post_titles = [item['data']['title']
+                           for item in res.json()['data']['children']
+                           if item['kind'] == 't3']
+            for title in post_titles:
+                print(title)
     else:
+        print("None")
         return 0
