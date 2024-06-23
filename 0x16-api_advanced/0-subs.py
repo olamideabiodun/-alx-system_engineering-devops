073101c2d3e912af9c6ef17aca142679196feb11
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-06-23 12:47:05 +0300

    checker

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,14 +3,16 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """gets number of subscribers to a certain reddit"""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent':
-               'script:subs/1.0 by /u/faruqolamide'}
-    
+               'script:subs1.0 by /u/faruqolamide'}
+
     res = requests.get(url, headers=headers, allow_redirects=False)
+
     if res.status_code == 200:
         return res.json()['data']['subscribers']
     else:
