a6c7ce82c53f1d88af509954c318b841ce479f90
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-07-04 18:42:27 +0300

    puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file
 file {'/tmp/school':
-	mode	=>'0744',
+    mode	=>'0744',
 	owner	=> 'www-data',
 	group	=> 'www-data',
-	content	=> 'I love Puppet'
+	content	=> 'I love Puppet',
 }
